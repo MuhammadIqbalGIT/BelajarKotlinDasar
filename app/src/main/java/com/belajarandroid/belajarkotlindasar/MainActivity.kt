26e70cd9d52a88bb2db32bf110726ff39f422641
iqbal <iqbalvzr@gmail.com>
2022-08-09 12:54:50 +0700

    1

--- app/src/main/java/com/belajarandroid/belajarkotlindasar/MainActivity.kt
+++ app/src/main/java/com/belajarandroid/belajarkotlindasar/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.belajarandroid.belajarkotlindasar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val btnHelloWorld: Button = findViewById(R.id.btnHelloWorld)
 
+
+
+
+        btnHelloWorld.setOnClickListener {
+            val intent = Intent(this, HelloWorldActivity::class.java)
+            startActivity(intent)
+
+
+        }
     }
 }
